828a8042e39a31dea563fe6bc81b2acad3a86d99
Benjamin Tidor <benjamin@tidor.net>
2022-11-09 16:54:24 -0800

    implement bit-shifting opcodes

--- opcodes.py
+++ opcodes.py
@@ -97,3 +97,80 @@ def SIGNEXTEND(b: uint256, x: uint256) -> uint256:
     if x & (1 << (bits - 1)) == 0:
         return x
     return ((MAX - 1) ^ ((1 << bits) - 1)) | x
+
+
+# 10 - Less-than comparison
+def LT(a: uint256, b: uint256) -> uint256:
+    return 1 if a < b else 0
+
+
+# 11 - Greater-than comparison
+def GT(a: uint256, b: uint256) -> uint256:
+    return 1 if a > b else 0
+
+
+# 12 - Signed less-than comparison
+def SLT(a: uint256, b: uint256) -> uint256:
+    a = _twos_complement(a)
+    b = _twos_complement(b)
+    return 1 if a < b else 0
+
+
+# 13 - Signed greater-than comparison
+def SGT(a: uint256, b: uint256) -> uint256:
+    a = _twos_complement(a)
+    b = _twos_complement(b)
+    return 1 if a > b else 0
+
+
+# 14 - Equality comparison
+def EQ(a: uint256, b: uint256) -> uint256:
+    return 1 if a == b else 0
+
+
+# 15 - Simple not operator
+def ISZERO(a: uint256) -> uint256:
+    return 1 if a == 0 else 0
+
+
+# 16 - Bitwise AND operation
+def AND(a: uint256, b: uint256) -> uint256:
+    return a & b
+
+
+# 17 - Bitwise OR operation
+def OR(a: uint256, b: uint256) -> uint256:
+    return a | b
+
+
+# 18 - Bitwise XOR operation
+def XOR(a: uint256, b: uint256) -> uint256:
+    return a ^ b
+
+
+# 19 - Bitwise NOT operation
+def NOT(a: uint256) -> uint256:
+    return a ^ (MAX - 1)
+
+
+# 1A - Retrieve single bytes from word
+def BYTE(i: uint256, x: uint256) -> uint256:
+    if i > 31:
+        return 0
+    return (x >> (8 * (31 - i))) & 0xFF
+
+
+# 1B - Left shift operation
+def SHL(shift: uint256, value: uint256) -> uint256:
+    return (value << shift) & (MAX - 1)
+
+
+# 1C - Logical right shift operation
+def SHR(shift: uint256, value: uint256) -> uint256:
+    return value >> shift
+
+
+# 1D - Arithmetic (signed) right shift operation
+def SAR(shift: uint256, value: uint256) -> uint256:
+    value = _twos_complement(value)
+    return _twos_complement(value >> shift)
--- test_opcodes.py
+++ test_opcodes.py
@@ -94,3 +94,85 @@ def test_SIGNEXTEND() -> None:
         == 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFAA
     )
     assert SIGNEXTEND(0, 0x7F) == 0x7F
+
+
+def test_LT() -> None:
+    assert LT(9, 10) == 1
+    assert LT(10, 10) == 0
+
+
+def test_GT() -> None:
+    assert GT(10, 9) == 1
+    assert GT(10, 10) == 0
+
+
+def test_SLT() -> None:
+    assert (
+        SLT(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, 0) == 1
+    )
+    assert SLT(10, 10) == 0
+
+
+def test_SGT() -> None:
+    assert (
+        SGT(0, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) == 1
+    )
+    assert SGT(10, 10) == 0
+
+
+def test_EQ() -> None:
+    assert EQ(10, 10) == 1
+    assert EQ(10, 5) == 0
+
+
+def test_ISZERO() -> None:
+    assert ISZERO(10) == 0
+    assert ISZERO(0) == 1
+
+
+def test_AND() -> None:
+    assert AND(0xF, 0xF) == 0xF
+    assert AND(0xFF, 0) == 0
+
+
+def test_OR() -> None:
+    assert OR(0xF0, 0xF) == 0xFF
+    assert OR(0xFF, 0xFF) == 0xFF
+
+
+def test_XOR() -> None:
+    assert XOR(0xF0, 0xF) == 0xFF
+    assert XOR(0xFF, 0xFF) == 0
+
+
+def test_NOT() -> None:
+    assert NOT(0) == 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+
+def test_BYTE() -> None:
+    assert BYTE(31, 0xFF) == 0xFF
+    assert BYTE(30, 0xFF00) == 0xFF
+    assert BYTE(30, 0xAABBCC) == 0xBB
+    assert BYTE(123456, 0xAABBCC) == 0
+
+
+def test_SHL() -> None:
+    assert SHL(1, 1) == 2
+    assert (
+        SHL(4, 0xFF00000000000000000000000000000000000000000000000000000000000000)
+        == 0xF000000000000000000000000000000000000000000000000000000000000000
+    )
+
+
+def test_SHR() -> None:
+    assert SHR(1, 2) == 1
+    assert SHR(4, 0xFF) == 0xF
+    assert SHR(123, 0xAA) == 0
+
+
+def test_SAR() -> None:
+    assert SAR(1, 2) == 1
+    assert (
+        SAR(4, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0)
+        == 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+    )
