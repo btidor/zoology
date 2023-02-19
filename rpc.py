96eff58b2d676e0e24a38add14a968d42cf85511
Benjamin Tidor <benjamin@tidor.net>
2023-02-18 16:38:12 -0800

    basic geth rpc client

---
+++ rpc.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+import requests
+
+from disassembler import Program, disassemble
+from smt import Uint160, Uint256
+
+# For now, make requests for a given, fixed block offset.
+TAG = "0x81f21d"
+
+API_KEY = "***"
+
+
+def get_code(address: Uint160) -> Optional[Program]:
+    """Load the Program at a given address."""
+    code = _api_request(
+        "eth_getCode",
+        address=address.unwrap(bytes).hex(),
+        tag=TAG,
+    )
+    return disassemble(code)
+
+
+def get_storage(address: Uint160, position: Uint256) -> Uint256:
+    """Load the contents of a given storage slot."""
+    value = _api_request(
+        "eth_getStorageAt",
+        address=address.unwrap(bytes).hex(),
+        position=position.unwrap(bytes).hex(),
+        tag=TAG,
+    )
+    return Uint256(int.from_bytes(value))
+
+
+def _api_request(action: str, **kwargs: str) -> bytes:
+    res = requests.get(
+        "https://api-goerli.etherscan.io/api",
+        {
+            **kwargs,
+            "module": "proxy",
+            "action": action,
+            "apikey": API_KEY,
+        },
+    )
+    res.raise_for_status()
+    assert "error" not in res.json(), res.json()["error"]["message"]
+    result = res.json()["result"]
+    if result[:2] != "0x":
+        raise ValueError(result)
+    return bytes.fromhex(result[2:])
+
+
+if __name__ == "__main__":
+    print(get_code(Uint160(0xD2E5E0102E55A5234379DD796B8C641CD5996EFD)))
+    print(
+        get_storage(
+            Uint160(0xD2E5E0102E55A5234379DD796B8C641CD5996EFD),
+            Uint256(0xD362A90B918E441DEC884EE15A0F6D394D7875F3929A29069CD25AFF7D935874),
+        ).describe()
+    )
